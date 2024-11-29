6c659c4b051312bcdd1ffa1ba24bdaad2b455401
Sumanth Rajkumar <rajkumar.sumanth@gmail.com>
2024-11-29 10:18:35 -0500

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from profiles_api import serializers
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -58,7 +61,7 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
-        
+
     def create(self, request):
             """Create a new hello message."""
             serializer = self.serializer_class(data=request.data)
@@ -93,4 +96,11 @@ class HelloViewSet(viewsets.ViewSet):
 
         return Response({'http_method': 'DELETE'})
 
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
 # Create your views here.
